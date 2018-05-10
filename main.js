8d4acdc61a177df814c589aed9cb75907b1f4e67
Aj <antwauntune25@wcpsstudent.org>
2018-05-10 01:24:11 -0400

    make it look prettier

--- main.js
+++ main.js
@@ -1,63 +1,67 @@
 window.onload = function() {
-var Button = document.getElementById("clickButton");
-var pingText = document.getElementById("pingText");
-var avgText = document.getElementById("avgPing");
-var pings = [];
-function ping(SERVER) {
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = function() {
-        if (http.readyState === http.HEADERS_RECEIVED) {
-            http.abort();
-            var totalPings = 0;
-            var ping = new Date - PING;
-          pingText.textContent = ping +" ms";
-            pings.push(ping);
-            for(var i = 0; i < pings.length; i++){
-                totalPings += pings[i];
+    var Button = document.getElementById("clickButton");
+    var pingText = document.getElementById("pingText");
+    var avgText = document.getElementById("avgPing");
+    var pings = [];
+
+    function ping(SERVER) {
+        var http = new XMLHttpRequest();
+        http.onreadystatechange = function() {
+            if (http.readyState === http.HEADERS_RECEIVED) {
+                http.abort();
+                var totalPings = 0;
+                var ping = new Date - PING;
+                pingText.textContent = ping + " ms";
+                pings.push(ping);
+                for (var i = 0; i < pings.length; i++) {
+                    totalPings += pings[i];
+                }
+                var avgPing = Math.round(totalPings / pings.length);
+                avgText.textContent = "Average Ping: " + avgPing;
             }
-            var avgPing = Math.round(totalPings / pings.length);
-            avgText.textContent = "Average Ping: "+avgPing;
+        };
+        google.charts.load('current', {
+            packages: ['corechart', 'line']
+        });
+        google.charts.setOnLoadCallback(drawBackgroundColor);
+
+        function drawBackgroundColor() {
+            var data = new google.visualization.DataTable();
+            data.addColumn('number');
+            data.addColumn('number');
+            for (var i = 0; i < pings.length; i++) {
+                data.addRows([
+                    [pings[i], new Date().getSeconds()]
+                ]);
+            }
+
+
+            var options = {
+                hAxis: {
+                    title: 'Ping'
+                },
+                vAxis: {
+                    title: 'Seconds'
+                },
+                backgroundColor: '#f1f8e9'
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
         }
-    };
-google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawBackgroundColor);
+        var PING = new Date;
 
-function drawBackgroundColor() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('number');
-      data.addColumn('number');
-    for(var i = 0; i < pings.length; i++){
-              data.addRows([
-        [pings[i], new Date().getSeconds()]
-      ]);
+        http.open(SERVER, "/");
+        http.send(null);
+
     }
 
-
-      var options = {
-        hAxis: {
-          title: 'Ping'
-        },
-        vAxis: {
-          title: 'Seconds'
-        },
-        backgroundColor: '#f1f8e9'
-      };
-
-      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
+    function NAstartPinging() {
+        clearInterval(interval);
+        var interval = setInterval(function() {
+            ping("104.160.131.3")
+        }, 1000);
     }
-    var PING = new Date;
 
-    http.open(SERVER, "/");
-    http.send(null);
-    
-}
-function NAstartPinging(){
-    clearInterval(interval);
-    var interval = setInterval(function() {
-        ping("104.160.131.3")
-    }, 1000);
-}
-
-Button.addEventListener('click', NAstartPinging, false)
+    Button.addEventListener('click', NAstartPinging, false)
 }
